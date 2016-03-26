b78191af7a286abfc9f1543a80c94f64d8720a31
amoshong <amos.hong@gmail.com>
2016-03-26 20:11:08 +0800

    Added files via upload

--- plot3.R
+++ plot3.R
@@ -1,11 +1,11 @@
 library(dplyr)
 ## Read datafrom 2007/2/1 to2007/2/2
-tmp=read.table("./R/Data/household_power_consumption.txt",sep=";",header = TRUE,nrows=1)
+tmp=read.table("./R/Data/household_power_consumption.txt",sep=";",na.strings = "?",header = TRUE,nrows=1)
 FirstDT=paste(tmp$Date , tmp$Time,sep=" ");
 tmpColNames=names(tmp);
 SkipNum<-unclass(strptime("1/2/2007 00:00:00","%d/%m/%Y %H:%M:%S")-strptime(FirstDT,"%d/%m/%Y %H:%M:%S"))*24*60 +1
 RowNum<-unclass(strptime("2/2/2007 23:59:00","%d/%m/%Y %H:%M:%S")-strptime("1/2/2007 00:00:00","%d/%m/%Y %H:%M:%S"))*24*60 + 1
-PowerData=read.table("./R/data/household_power_consumption.txt",skip=SkipNum,sep=";",header = F,nrows=RowNum,col.names = tmpColNames)
+PowerData=read.table("./R/data/household_power_consumption.txt",skip=SkipNum,sep=";",na.strings = "?",header = F,nrows=RowNum,col.names = tmpColNames)
 rm(tmp,tmpColNames,FirstDT,SkipNum,RowNum)
 #head(PowerData,1)
 #tail(PowerData,1)
@@ -13,6 +13,7 @@ rm(tmp,tmpColNames,FirstDT,SkipNum,RowNum)
 
 #convert date time 
 PowerData<-mutate(PowerData,DTs=as.POSIXct(strptime(paste(Date ,Time,sep=" "),"%d/%m/%Y %H:%M:%S")))
+
 
 # chinese os to english LC_Time
 Sys.setlocale("LC_TIME", "English")
--- plot4.R
+++ plot4.R
@@ -1,11 +1,11 @@
-
+library(dplyr)
 ## Read datafrom 2007/2/1 to2007/2/2
-tmp=read.table("./R/Data/household_power_consumption.txt",sep=";",header = TRUE,nrows=1)
+tmp=read.table("./R/Data/household_power_consumption.txt",sep=";",na.strings = "?",header = TRUE,nrows=1)
 FirstDT=paste(tmp$Date , tmp$Time,sep=" ");
 tmpColNames=names(tmp);
 SkipNum<-unclass(strptime("1/2/2007 00:00:00","%d/%m/%Y %H:%M:%S")-strptime(FirstDT,"%d/%m/%Y %H:%M:%S"))*24*60 +1
 RowNum<-unclass(strptime("2/2/2007 23:59:00","%d/%m/%Y %H:%M:%S")-strptime("1/2/2007 00:00:00","%d/%m/%Y %H:%M:%S"))*24*60 + 1
-PowerData=read.table("./R/data/household_power_consumption.txt",skip=SkipNum,sep=";",header = F,nrows=RowNum,col.names = tmpColNames)
+PowerData=read.table("./R/data/household_power_consumption.txt",skip=SkipNum,sep=";",na.strings = "?",header = F,nrows=RowNum,col.names = tmpColNames)
 rm(tmp,tmpColNames,FirstDT,SkipNum,RowNum)
 #head(PowerData,1)
 #tail(PowerData,1)
@@ -26,6 +26,7 @@ with(PowerData, {
     lines(DTs,Global_active_power)
 })
 
+
 with(PowerData, {
     plot(DTs,Voltage ,xlab="datetime",ylab="Voltage",type="n")
     lines(DTs,Voltage)
@@ -38,6 +39,7 @@ with(PowerData, {
     lines(DTs,Sub_metering_3,col="blue")
 })
 legend("topright",bty="n",lty=1, col = c("black","red", "blue"), legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+
 
 with(PowerData, {
     plot(DTs,Global_reactive_power ,xlab="datetime",ylab="Global_reactive_power",type="n")
